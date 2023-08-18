bce30a0d4ba9c7b313865c2bdc82cecbb62ebd52
andijayawizard <andijayawizard@gmail.com>
2023-08-18 14:23:14 +0700

    1808

--- app.vue
+++ app.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <Header />
-    <Navbar />
-    <NuxtPage />
+    <div class="w-full flex flex-col sm:flex-row flex-grow overflow-hidden">
+        <div class="sm:w-1/3 md:1/4 w-full flex-shrink flex-grow-0 p-4">
+            <div class="sticky top-0 p-4 bg-gray-100 rounded-xl w-full">
+              <Sidebar />
+            </div>
+            <div class="bg-gray-50 rounded-xl border my-3 w-full">
+                <div class="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                    <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        <span class="block text-indigo-600 overflow-ellipsis">Made with Tailwind CSS!</span>
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <main role="main" class="w-full h-full flex-grow p-3 overflow-auto">
+          <NuxtPage />
+        </main>
+    </div>
     <Footer />
   </div>
 </template>
---
+++ components/Sidebar.vue
@@ -0,0 +1,54 @@
+<template>
+	<ul class="flex sm:flex-col overflow-hidden content-center justify-between">
+		<li class="py-2 hover:bg-indigo-300 rounded">
+			<NuxtLink to="/">
+				<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
+						<span class="hidden sm:inline">Home</span>
+			</NuxtLink>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+			<NuxtLink to="/about-us">
+				<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
+						<span class="hidden sm:inline">About Us</span>
+			</NuxtLink>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+			<NuxtLink to="/products">
+				<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
+						<span class="hidden sm:inline">Products</span>
+			</NuxtLink>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+			<NuxtLink to="/blog">
+				<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
+						<span class="hidden sm:inline">Blog</span>
+			</NuxtLink>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+				<a class="truncate" href="#">
+						<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
+						<span class="hidden sm:inline">Home</span>
+				</a>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+				<a class="truncate" href="#">
+						<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/cog.svg" class="w-7 sm:mx-2 mx-4 inline" /> <span class="hidden sm:inline">Settings</span>
+				</a>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+				<a class="" href="#">
+						<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/gift.svg" class="w-7 sm:mx-2 mx-4 inline" /> <span class="hidden sm:inline">Products</span>
+				</a>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+				<a class="" href="#">
+						<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/chart-bar.svg" class="w-7 sm:mx-2 mx-4 inline" /> <span class="hidden sm:inline">Reports</span>
+				</a>
+		</li>
+		<li class="py-2 hover:bg-indigo-300 rounded">
+				<a class="" href="#">
+						<img src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/collection.svg" class="w-7 sm:mx-2 mx-4 inline" /> <span class="hidden sm:inline">Integrations</span>
+				</a>
+		</li>
+	</ul>
+</template>
