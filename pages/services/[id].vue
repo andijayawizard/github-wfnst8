b5fa767794a5bbbabc7635989a8b5e3e1f66ed67
andijayawizard <andijayawizard@gmail.com>
2023-08-21 21:51:58 +0700

    2108

---
+++ pages/services/[id].vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="pending">loading...</div>
+  <div v-else>
+    <img
+      :src="`${config.public.apiUrlBitu}/assets/uploads/images/services/${item.acak}-1.jpg`"
+    />
+    <h1 class="text-3xl">{{ item.nama }}</h1>
+    <p>{{ item.ktrg }}</p>
+  </div>
+</template>
+<script setup>
+useHead({
+  title: "Blog Details",
+});
+const config = useRuntimeConfig();
+const route = useRoute();
+const { data: item, pending } = await useFetch(
+  `${config.public.apiUrlBitu}/api/api.php/records/services/${route.params.id}`,
+  {
+    headers: {
+      "x-api-key": config.public.apiKeyBitu,
+    },
+  }
+);
+</script>
