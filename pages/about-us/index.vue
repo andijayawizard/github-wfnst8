65fc188f27c7008cde52cf26d7f06cae002bd987
andijayawizard <andijayawizard@gmail.com>
2023-08-21 15:12:54 +0700

    2108

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -1,4 +1,16 @@
 <template>
   <h1 class="text-3xl">about us</h1>
-  <p>we are here to help you</p>
+  <div>
+    <p v-if="pending">loading...</p>
+    <p v-else>{{ item.ktrg }}</p>
+    </div>
 </template>
+<script setup>
+const config = useRuntimeConfig()
+const apiUrl = config.public.apiUrlBitu
+const {data: item, pending} = await useLazyAsyncData('about', ()=>$fetch(`${apiUrl}/api/api.php/records/menuweb/2`, {
+  headers: {
+    'x-api-key': config.public.apiKeyBitu
+  }
+}))
+</script>
