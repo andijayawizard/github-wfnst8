2d9f870e6a57dccb472ba238477c8f09918c967b
andijayawizard <andijayawizard@gmail.com>
2023-08-21 12:44:18 +0700

    2108

---
+++ pages/services/[slug].vue
@@ -0,0 +1,10 @@
+<template>details services</template>
+<script setup>
+const config=useRuntimeConfig()
+const apiUrl=config.public.apiUrlBitu
+	const {data: item}=await useFetch(`${apiUrl}/api/api.php/services/${item.seo}`, {
+		headers: {
+			'x-api-key': config.public.apiKeyBitu
+		}
+	})
+</script>
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -2,31 +2,51 @@
   <h1 class="text-3xl">Our Services</h1>
   <div v-if="pending">loading...</div>
   <div v-else class="flex flex-wrap">
-    <div v-for="item in list.records" :key="item.id" class="max-w-sm rounded overflow-hidden shadow-lg m-2">
-      <img class="w-full" :src="`${apiUrl}/assets/uploads/images/services/${item.acak}-1.jpg`" :alt="item.nama">
+    <div
+      v-for="item in list.records"
+      :key="item.id"
+      class="max-w-sm rounded overflow-hidden shadow-lg m-2"
+    >
+      <img
+        class="w-full"
+        :src="`${apiUrl}/assets/uploads/images/services/${item.acak}-1.jpg`"
+        :alt="item.nama"
+      />
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{{item.nama}}</div>
+        <div class="font-bold text-xl mb-2">
+          <NuxtLink :to="`services/${item.seo}`">{{ item.nama }}</NuxtLink>
+        </div>
         <p class="text-gray-700 text-base">
-          {{item.rgks}}
+          {{ item.rgks }}
         </p>
       </div>
       <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#photography</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#travel</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#winter</span
+        >
       </div>
     </div>
   </div>
-
 </template>
 
 <script setup>
-  const config=useRuntimeConfig()
-  const apiUrl=config.public.apiUrlBitu
-  const params='/api/api.php/records/services?filter=pub,eq,1'
-  const {data: list, error, pending, refresh}=await useLazyAsyncData('services', ()=>$fetch(`${apiUrl}${params}`, {
+const config = useRuntimeConfig();
+const apiUrl = config.public.apiUrlBitu;
+const params = "/api/api.php/records/services?filter=pub,eq,1";
+const { data: list, error, pending, refresh } = await useLazyAsyncData("services", () =>
+  $fetch(`${apiUrl}${params}`, {
     headers: {
-      'x-api-key': config.public.apiKeyBitu
-    }
-  }))
+      "x-api-key": config.public.apiKeyBitu,
+    },
+  })
+);
 </script>
