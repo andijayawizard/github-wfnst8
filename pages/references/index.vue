1b4294b07cbf6bd03da5b9dad45a8bc04c191b95
andijayawizard <andijayawizard@gmail.com>
2023-08-22 17:27:26 +0700

    2208

--- pages/references/index.vue
+++ pages/references/index.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h1 class="text-3xl">references</h1>
-        <p>our references</p>
+  <div>
+    <h1 class="text-3xl">references</h1>
+    <p>our references</p>
+    <div v-if="pending">loading...</div>
+    <div v-else>
+      <div v-for="item in list.records">{{ item.nama }}</div>
     </div>
+  </div>
 </template>
+<script>
+const config = useRunfimeConfig();
+const { data: list, pending } = await useAsyncData("references", () =>
+  $fetch(`${config.public.apiUrlBitu}/api/api.php/records/references`, {
+    headers: {
+      "x-api-key": config.public.apiKeyBitu,
+    },
+  })
+);
+</script>
