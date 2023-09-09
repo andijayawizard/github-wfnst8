7cf400aa4d46351acf5d59c1408c7cc5dc12eb68
andijayawizard <andijayawizard@gmail.com>
2023-09-09 21:30:32 +0700

    0909

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
 const config = useRuntimeConfig();
 const { data: list } = await useLazyAsyncData("menuweb", () =>
-  $fetch(
-    `${config.public.apiUrlBitu}/api/api.php/records/menuweb?filter=pub,eq,1`,
-    // `${config.public.apiUrlLocal}/api/menuweb`,
-    {
-      headers: {
-        "x-api-key": config.public.apiKeyBitu,
-      },
-    }
-  )
+  $fetch(`/api/api.php/records/menuweb?filter=pub,eq,1`, {
+    baseURL: config.public.apiUrlBitu,
+    headers: {
+      "x-api-key": config.public.apiKeyBitu,
+    },
+  })
+);
+const { data: services } = await useLazyAsyncData("services", () =>
+  $fetch(`/api/api.php/records/services?filter=pub,eq,1`, {
+    baseURL: config.public.apiUrlBitu,
+    headers: {
+      "x-api-key": config.public.apiKeyBitu,
+    },
+  })
 );
 </script>
 
@@ -35,22 +40,16 @@ const { data: list } = await useLazyAsyncData("menuweb", () =>
                 id="navbarToggler"
                 class="block absolute right-4 top-1/2 -translate-y-1/2 lg:hidden focus:ring-2 ring-primary px-3 py-[6px] rounded-lg"
               >
-                <span
-                  class="relative w-[30px] h-[2px] my-[6px] block bg-white"
-                ></span>
-                <span
-                  class="relative w-[30px] h-[2px] my-[6px] block bg-white"
-                ></span>
-                <span
-                  class="relative w-[30px] h-[2px] my-[6px] block bg-white"
-                ></span>
+                <span class="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
+                <span class="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
+                <span class="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
               </button>
               <nav
                 id="navbarCollapse"
                 class="absolute py-5 lg:py-0 lg:px-4 xl:px-6 bg-white lg:bg-transparent shadow-lg rounded-lg max-w-[250px] w-full lg:max-w-full lg:w-full right-4 top-full hidden lg:block lg:static lg:shadow-none"
               >
-                <ul class="blcok lg:flex">
-                  <li class="relative group">
+                <ul class="block lg:flex">
+                  <!-- <li class="relative group">
                     <NuxtLink
                       v-for="(item, index) in list.records"
                       :key="index"
@@ -59,101 +58,44 @@ const { data: list } = await useLazyAsyncData("menuweb", () =>
                     >
                       {{ item.caption }}
                     </NuxtLink>
-                  </li>
+                  </li> -->
                   <!-- <li class="relative group">
                     <NuxtLink
                       class="ud-menu-scroll text-base text-dark lg:text-white lg:group-hover:opacity-70 lg:group-hover:text-white group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-7 xl:ml-12"
                       to="/about-us"
                       >About</NuxtLink
                     >
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="#pricing"
-                      class="ud-menu-scroll text-base text-dark lg:text-white lg:group-hover:opacity-70 lg:group-hover:text-white group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-7 xl:ml-12"
-                    >
-                      Pricing
-                    </a>
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="#team"
-                      class="ud-menu-scroll text-base text-dark lg:text-white lg:group-hover:opacity-70 lg:group-hover:text-white group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-7 xl:ml-12"
-                    >
-                      Team
-                    </a>
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="#contact"
-                      class="ud-menu-scroll text-base text-dark lg:text-white lg:group-hover:opacity-70 lg:group-hover:text-white group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:px-0 flex mx-8 lg:mr-0 lg:ml-7 xl:ml-12"
-                    >
-                      Contact
-                    </a>
                   </li> -->
-                  <li class="relative group submenu-item">
-                    <a
-                      href="javascript:void(0)"
-                      class="text-base text-dark lg:text-white lg:group-hover:opacity-70 lg:group-hover:text-white group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12 relative after:absolute after:w-2 after:h-2 after:border-b-2 after:border-r-2 after:border-current after:rotate-45 lg:after:right-0 after:right-1 after:top-1/2 after:-translate-y-1/2 after:mt-[-2px]"
+                  <li
+                    v-for="(item, index) in list.records"
+                    :key="index"
+                    class="relative group"
+                    :class="item.nama == 'services' ? 'submenu-item' : ''"
+                  >
+                    <NuxtLink
+                      :to="
+                        item.nama == 'services' ? 'javascript:void(0)' : `/${item.seo}`
+                      "
+                      v-text="item.caption"
+                      class="text-base text-dark lg:text-white lg:group-hover:opacity-70 lg:group-hover:text-white group-hover:text-primary py-2 lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4 flex mx-8 lg:mr-0 lg:ml-8 xl:ml-12"
+                      :class="
+                        item.nama == 'services'
+                          ? 'relative after:absolute after:w-2 after:h-2 after:border-b-2 after:border-r-2 after:border-current after:rotate-45 lg:after:right-0 after:right-1 after:top-1/2 after:-translate-y-1/2 after:mt-[-2px]'
+                          : ''
+                      "
                     >
-                      Pages
-                    </a>
+                    </NuxtLink>
                     <div
+                      v-show="item.nama == 'services'"
                       class="submenu hidden relative lg:absolute w-[250px] top-full lg:top-[110%] left-0 rounded-sm lg:shadow-lg p-4 lg:block lg:opacity-0 lg:invisible group-hover:opacity-100 lg:group-hover:visible lg:group-hover:top-full bg-white transition-[top] duration-300"
                     >
                       <NuxtLink
-                        to="/about-us"
+                        v-for="(item, index) in services.records"
+                        :key="index"
+                        :to="`/services/${item.id}`"
+                        v-text="item.nama"
                         class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                        >About</NuxtLink
-                      >
-                      <a
-                        href="about.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        About Page
-                      </a>
-                      <a
-                        href="pricing.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        Pricing Page
-                      </a>
-                      <a
-                        href="contact.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        Contact Page
-                      </a>
-                      <a
-                        href="blog-grids.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        Blog Grid Page
-                      </a>
-                      <a
-                        href="blog-details.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        Blog Details Page
-                      </a>
-                      <a
-                        href="signup.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        Sign Up Page
-                      </a>
-                      <a
-                        href="signin.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        Sign In Page
-                      </a>
-                      <a
-                        href="404.html"
-                        class="block text-sm text-body-color rounded hover:text-primary py-[10px] px-4"
-                      >
-                        404 Page
-                      </a>
+                      ></NuxtLink>
                     </div>
                   </li>
                 </ul>
